2d3788b417aa1da1b96b4ea4cdaca8f90b627467
EthanYLeong <ethanleong26@yahoo.com>
2025-11-11 18:19:22 -0500

    add gui with note, cents, hz, very sketchy

--- src/Gui.java
+++ src/Gui.java
@@ -5,25 +5,35 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 public class Gui {
 
     int frameWidth = 600;
     int frameHeight = 600;
     JFrame frame = new JFrame("goated tuner");
-    JPanel textPanel = new JPanel();
+
+    JPanel titlePanel = new JPanel();
+    JPanel noteAndCentsPanel = new JPanel();
     JPanel frequencyPanel = new JPanel();
+
     JLabel title = new JLabel();
     JLabel frequencyText = new JLabel();
+    JLabel noteNameText = new JLabel();
+    JLabel centsLabelText = new JLabel();
+    JLabel centsValueText = new JLabel();
+    
 
+    GridBagConstraints constraints = new GridBagConstraints();
 
     Gui () {
         frame.setVisible(true);
         frame.setSize(frameWidth, frameHeight);
         frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
+        frame.setLayout(new GridBagLayout());
         frame.setBackground(Color.darkGray);
         frame.getContentPane().setBackground(Color.darkGray);
 
@@ -34,6 +44,13 @@ public class Gui {
         title.setText("epic tuner");
         title.setOpaque(true);
 
+        noteNameText.setBackground(Color.gray);
+        noteNameText.setForeground(Color.white);
+        noteNameText.setHorizontalAlignment(JLabel.CENTER);
+        noteNameText.setFont(new Font("Arial", Font.BOLD, 200));
+        noteNameText.setText("A");
+        noteNameText.setOpaque(true);
+
         frequencyText.setBackground(Color.gray);
         frequencyText.setForeground(Color.white);
         frequencyText.setHorizontalAlignment(JLabel.CENTER);
@@ -41,27 +58,87 @@ public class Gui {
         frequencyText.setText("100");
         frequencyText.setOpaque(true);
 
-        textPanel.setLayout(new BorderLayout());
-        textPanel.add(title);
+        centsLabelText.setBackground(Color.gray);
+        centsLabelText.setForeground(Color.white);
+        centsLabelText.setHorizontalAlignment(JLabel.CENTER);
+        centsLabelText.setVerticalAlignment(JLabel.BOTTOM);
+        centsValueText.setFont(new Font("Arial", Font.BOLD, 100));
+        centsLabelText.setText("CENTS: ");
+        
+        centsValueText.setBackground(Color.gray);
+        centsValueText.setForeground(Color.white);
+        centsValueText.setHorizontalAlignment(JLabel.CENTER);
+        centsValueText.setFont(new Font("Arial", Font.BOLD, 100));
+        centsValueText.setText("1");
+        centsValueText.setOpaque(true);
 
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.add(title);
+
+        noteAndCentsPanel.setLayout(new BorderLayout());
+        noteAndCentsPanel.add(noteNameText);
+        noteAndCentsPanel.add(centsValueText, BorderLayout.EAST);
+        
         frequencyPanel.setLayout(new BorderLayout());
         frequencyPanel.add(frequencyText);
+
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1.0;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        frame.add(titlePanel, constraints);
+
+        constraints.fill = GridBagConstraints.BOTH;
+        // constraints.weightx = 1.0;
+        constraints.gridy = 1;
+        constraints.gridx = 0;
+        constraints.weighty = 1;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 2;
+        frame.add(noteNameText, constraints);
         
 
-        frame.add(textPanel, BorderLayout.NORTH);
-        frame.add(frequencyPanel);
+        constraints.gridy = 1;
+        constraints.gridx = 1;
+        constraints.weighty = 0;
+        constraints.weighty = 0;
+        constraints.gridheight = 1;
+        frame.add(centsLabelText, constraints);
+
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridy = 2;
+        constraints.gridx = 1;
+        constraints.weighty = 1;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        frame.add(centsValueText, constraints);
+
+
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1.0;
+        constraints.weighty = 0;
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        frame.add(frequencyPanel, constraints);
     }
 
-    void updateFrequency(double frequency, boolean loudEnough){
+    void updateFrequency(double frequency, String noteAndOctave, double cents){
         frequencyText.setText(Double.toString(frequency));
+        noteNameText.setText(noteAndOctave);
+        centsValueText.setText(Double.toString(cents));
+
+
     }
 
-    void loudEnoughColor(boolean loudEnough){
+    public void loudEnoughColor(boolean loudEnough) {
         if (loudEnough){
             frequencyText.setBackground(Color.green);
         } else {
             frequencyText.setBackground(Color.red);
-        }
-    }
+        }    }
+
+
 
 }
--- src/Tuner.java
+++ src/Tuner.java
@@ -1,4 +1,5 @@
 import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
@@ -23,7 +24,33 @@ public class Tuner {
     DoubleFFT_1D FFT = new DoubleFFT_1D(22050);
     boolean loudEnough = false;
 
+    HashMap<Integer, String> notesMap = new HashMap<Integer, String>();
+
     Tuner(Gui gui) {
+        notesMap.put(0, "A");
+        notesMap.put(-11, "A#");
+        notesMap.put(1, "A#");
+        notesMap.put(-10, "B");
+        notesMap.put(2, "B");
+        notesMap.put(-9, "C");
+        notesMap.put(3, "C");
+        notesMap.put(-8, "C#");
+        notesMap.put(4, "C#");
+        notesMap.put(-7, "D");
+        notesMap.put(5, "D");
+        notesMap.put(-6, "D#");
+        notesMap.put(6, "D#");
+        notesMap.put(-5, "E");
+        notesMap.put(7, "E");
+        notesMap.put(-4, "F");
+        notesMap.put(8, "F");
+        notesMap.put(-3, "F#");
+        notesMap.put(9, "F#");
+        notesMap.put(-2, "G");
+        notesMap.put(10, "G");
+        notesMap.put(-1, "G#");
+        notesMap.put(11, "G#");
+
         this.gui = gui;
 
         if (!AudioSystem.isLineSupported(info)) {
@@ -63,7 +90,7 @@ public class Tuner {
                     largestAmplitude = Math.abs(doubleArray[i]);
                 }
             }
-            if (largestAmplitude < 1000){
+            if (largestAmplitude < 750){
                 loudEnough = false;
                 gui.loudEnoughColor(loudEnough);
                 continue;
@@ -87,20 +114,43 @@ public class Tuner {
 
             }
             // num of cycles over time period / (seconds of time period)
-            double frequency =(mostFrequentBin * 44100.0)/doubleArray.length;
+            double frequency = (mostFrequentBin * 44100.0)/doubleArray.length;
             double numOfSemitones = 12 * (Math.log((frequency/440))/Math.log(2));
             System.out.println(frequency + " semit " + numOfSemitones);
             double lower = Math.floor(numOfSemitones);
             double upper = Math.ceil(numOfSemitones);
+            double closest = 0;
+            String noteName = "";
             if (numOfSemitones - lower > upper - numOfSemitones){
-                double closest = upper;
+                closest = upper;
             } else {
-                double closest = lower;
+                closest = lower;
             }
 
-             
+            int semitoneDifference = (int) closest % 12;
 
-            gui.updateFrequency(frequency, loudEnough);
+            noteName = notesMap.get(semitoneDifference);
+
+            double correctPitchFrequency = 440 * Math.pow(1.05946, closest);
+            double cents = 1200 * (Math.log(frequency/correctPitchFrequency)/Math.log(2));
+            cents = Math.floor(cents * 10)/10;
+            double octave = 4;
+            // had to artificially lower C4 in order for C6 to be caputred
+            double octaveDifference = Math.log(frequency/261)/Math.log(2);
+            if (octaveDifference >= 0){
+                octaveDifference = Math.floor(octaveDifference);
+                octave += octaveDifference;
+            } else {
+                octaveDifference = 1/octaveDifference;
+                octaveDifference = Math.ceil(octaveDifference);
+                octave -= octaveDifference;
+            }
+
+            System.out.println(octave);
+
+            String noteAndOctave = "<html>" + noteName + "<sub>" + (int)(octave) + "</sub></html>";
+
+            gui.updateFrequency(frequency, noteAndOctave, cents);
             // out.write(data, 0, numBytesRead); 
             // System.out.println(out);
         }
